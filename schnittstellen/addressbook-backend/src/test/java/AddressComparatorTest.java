351a6f38e4a7cc651589ae3909dc3279b10cb148
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-01-11 16:06:44 +0100

    Update AddressComparatorTest

--- schnittstellen/addressbook-backend/src/test/java/AddressComparatorTest.java
+++ schnittstellen/addressbook-backend/src/test/java/AddressComparatorTest.java
@@ -8,11 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddressComparatorTest {
     @Test
-    public void testCompare() {
+    public void testCompareDifferentLastNames() {
+        AddressComparator comparator = new AddressComparator();
+        Address address1 = new Address(1, "Max", "Doe", "1234567890", new Date());
+        Address address2 = new Address(2, "John", "Mustermann", "0987654321", new Date());
+        
+        assertTrue(comparator.compare(address1, address2) < 0);
+    }
+
+    @Test
+    public void testCompareSameLastNamesDifferentIds() {
         AddressComparator comparator = new AddressComparator();
         Address address1 = new Address(1, "Max", "Mustermann", "1234567890", new Date());
-        Address address2 = new Address(2, "John", "Doe", "0987654321", new Date());
+        Address address2 = new Address(2, "Max", "Mustermann", "0987654321", new Date());
 
         assertTrue(comparator.compare(address1, address2) < 0);
     }
 }
+

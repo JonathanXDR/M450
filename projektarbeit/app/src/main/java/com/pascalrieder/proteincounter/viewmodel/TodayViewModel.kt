05c74fde03d3342b1192a1ffdae138ff13e59137
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-01-24 16:39:19 +0100

    Update TodayViewModel

--- projektarbeit/app/src/main/java/com/pascalrieder/proteincounter/viewmodel/TodayViewModel.kt
+++ projektarbeit/app/src/main/java/com/pascalrieder/proteincounter/viewmodel/TodayViewModel.kt
@@ -3,6 +3,7 @@ package com.pascalrieder.proteincounter.viewmodel
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -57,7 +58,7 @@ class TodayViewModel(application: Application) : AndroidViewModel(application) {
                         )
                     )
                 } catch (e: Exception) {
-                    println(e)
+                    Log.e( "TodayViewModel", "Error while adding day", e)
                 }
             }
         })
